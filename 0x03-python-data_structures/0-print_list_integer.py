b33f99452ed32e9e498102559f01d6c89bfcb966
jabessa taye <jabesa4@gmail.com>
2023-01-12 11:22:58 -0800

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-# loop through each element in the list
-for num in my_list:
-# check if the element is an integer
-if type(num) == int:
-# print the element
-print(num)
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
