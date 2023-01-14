ced47b0b9c0d9394f5d4e388020ef3dd0db024eb
jabessa taye <jabesa4@gmail.com>
2023-01-14 01:26:46 -0800

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_int = my_list[0]
+    for num in my_list:
+        if num > max_int:
+            max_int = num
+    return max_int
